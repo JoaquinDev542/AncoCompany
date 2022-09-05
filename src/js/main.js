d9dbf768f8d5f123b9983b2b971244867537007a
MiguelAngel <ansonfull@gmail.com>
2022-09-06 01:44:12 +0200

    loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 // Loader
+window.addEventListener('load',function () {  
+
+    let main = document.querySelector('main')
+
+    main.classList.add("hide");
+
+    const myTimeout = setTimeout(myGreeting, 3000);
+
+    function myGreeting() {
+        document.getElementById("loader").classList.add("active");
+        main.classList.remove("hide");
+
+    }
+
+})
