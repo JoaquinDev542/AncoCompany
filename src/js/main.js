2f05746caa923a1e6ff83673f05ed3886ffe0db6
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-09-06 21:03:50 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ window.addEventListener('load',function () {
     const myTimeout = setTimeout(myGreeting, 2000);
 
     function myGreeting() {
-        document.getElementById("loader").classList.add("active");
+        document.querySelector(".loaderContainer").classList.add("active");
         main.classList.remove("hide");
 
     }
