1d8205b94c013626035d525759b8a179846fe867
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-09-06 23:00:19 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,39 @@
 // Loader
-// window.addEventListener('load',function () {  
+window.addEventListener('load',function () {  
 
-//     let main = document.querySelector('main')
+    // let main = document.querySelector('main')
 
-//     main.classList.add("hide");
+    // main.classList.add("hide");
 
-//     const myTimeout = setTimeout(myGreeting, 2000);
+    // const myTimeout = setTimeout(myGreeting, 2000);
 
-//     function myGreeting() {
-//         document.querySelector(".loaderContainer").classList.add("active");
-//         main.classList.remove("hide");
+    // function myGreeting() {
+    //     document.querySelector(".loaderContainer").classList.add("active");
+    //     main.classList.remove("hide");
+    // }
 
-//     }
+    // Menu Reponsive
+    let menuopen = document.querySelector(".menuOpen")
+    let menuclose = document.querySelector(".menuClose")
+    let menuCloseX = document.querySelector(".menuCloseX");
+    let menuCloseAll = document.querySelector(".menuCloseAll");
 
-// })
+
+    menuopen.addEventListener('click', function () {
+
+        menuclose.classList.add('active');
+        menuopen.classList.add('active');
+        menuCloseAll.classList.add('active');
+
+
+    })
+
+    menuCloseX.addEventListener('click', function () {
+
+        menuclose.classList.remove('active');
+        menuopen.classList.remove("active");
+
+    })
+
+})
+
