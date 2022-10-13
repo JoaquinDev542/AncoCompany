040249648c44d5822ce26a8601a9eb6bae6d7584
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-10-13 17:54:38 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -42,15 +42,12 @@ window.addEventListener('load',function () {
     
     ScrollReveal().reveal('.header', {origin: 'top'});
     ScrollReveal().reveal('.herotitle' , {origin: 'left'});
-    ScrollReveal().reveal('.wave');
     ScrollReveal().reveal('.heroContentContainer__btn');
-    ScrollReveal().reveal('.scrollZone');
-    ScrollReveal().reveal('.socialMedia--hero');
+    ScrollReveal().reveal('.wave');
     ScrollReveal().reveal('.aboutContentContainer__paragraph--SR-first', {origin: 'left'});
     ScrollReveal().reveal('.aboutContentContainer__paragraph--SR-second', {origin: 'left'});
     ScrollReveal().reveal('.aboutContentContainer__paragraph--SR-third', {origin: 'left'});
     ScrollReveal().reveal('.aboutContentContainer__paragraph--SR-fourth', {origin: 'left'});
-    // ScrollReveal().reveal('.informationContentFirst__paragraph--SR-first'); No existe
     ScrollReveal().reveal('.informationContentContainer--SR-first' , {delay: 200});
     ScrollReveal().reveal('.informationContentContainer--SR-second' , {delay: 400});
     ScrollReveal().reveal('.informationContentContainer--SR-third' , {delay: 600});
@@ -60,10 +57,6 @@ window.addEventListener('load',function () {
     ScrollReveal().reveal('.workProject--fourth');
     ScrollReveal().reveal('.workProject--fifth');
     ScrollReveal().reveal('.workProject--sixth');
-    // ScrollReveal().reveal('.quotesContainerTitles', {origin: 'left'});
-    // ScrollReveal().reveal('.quotesContainerBoxs');
-    // ScrollReveal().reveal('.quotesContainerArrows');
-    // ScrollReveal().reveal('.contact');
 
     // Menu Reponsive
     let menuopen = document.querySelector(".menuOpen")
