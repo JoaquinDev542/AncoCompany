3e80a8e153ff5e03ce5a7765326f64fe66f5dbe6
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-10-14 14:15:31 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -127,6 +127,25 @@ window.addEventListener('load',function () {
 
     })
 
+    // Btn Section Animation Javascript
+    let animateButton = function(e) {
+
+        e.preventDefault;
+        //reset animation
+        e.target.classList.remove('animate');
+        
+        e.target.classList.add('animate');
+        setTimeout(function(){
+          e.target.classList.remove('animate');
+        },700);
+        };
+        
+        let heroContentContainerBtn = document.getElementsByClassName("heroContentContainer__btn");
+        
+        for (let i = 0; i < heroContentContainerBtn.length; i++) {
+            heroContentContainerBtn[i].addEventListener('mouseover', animateButton, false);
+        }
+
     // Quotes Section
 
     // let quotesContainerArrowsLeft = document.querySelector(".quotesContainerArrowsLeft");
