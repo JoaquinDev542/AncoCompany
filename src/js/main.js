8174e2f7c221e857e0c39ae946918ae402d47aa3
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-10-20 22:10:48 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -105,24 +105,6 @@ window.addEventListener('load',function () {
         document.documentElement.classList.toggle("white-mode");
         
     }) 
-    
-    // const mediaQuery = '(max-width: 1024px)';
-    // const mediaQueryList = window.matchMedia(mediaQuery);
-    
-    // mediaQueryList.addEventListener('change', event => {
-    //     console.log(window.innerWidth);
-    //   if (event.matches) {
-    //         menuCloseAll.classList.remove('active');
-    //   } else {
-    //   }
-    // })
-
-    // if (window.matchMedia("(max-width: 1024px)") == false) {
-    //     menuCloseAll.classList.add('active');
-    //     menuclose.classList.remove('active');
-    //     menuclose.style.color = "red";
-
-    // }
 
     // Menu Responsive Links
     let workLink = document.querySelector("#workLink");
@@ -203,114 +185,8 @@ window.addEventListener('load',function () {
 
     asideBtn.addEventListener("click" , ()=> {
         aside__paragraphOpacity.classList.add("active");
+        asideBtn.classList.add("hidden");
     });
-
-
-
-
-    // Quotes Section
-
-    // let quotesContainerArrowsLeft = document.querySelector(".quotesContainerArrowsLeft");
-    // let quotesContainerArrowsRight = document.querySelector(".quotesContainerArrowsRight");
-    // let quotesContainerBoxFirst = document.querySelector(".quotesContainerBox--first");
-    // let quotesContainerBoxSecond = document.querySelector(".quotesContainerBox--second");
-    // let quotesContainerBoxThird = document.querySelector(".quotesContainerBox--third");
-    // let quotesContainerBoxFourth = document.querySelector(".quotesContainerBox--fourth");
-    // let quotesContainerBoxFifth = document.querySelector(".quotesContainerBox--fifth");
-
-    // setInterval(QuoteTransitionFirst, 5000);
-
-    // function QuoteTransitionFirst() {
-
-    //     if ((getComputedStyle(quotesContainerBoxFirst).getPropertyValue("display")) == "block"){
-
-    //         quotesContainerBoxFirst.style.display = "none";
-    //         quotesContainerBoxSecond.style.display = "block";
- 
-    //     } else if (getComputedStyle(quotesContainerBoxSecond).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxSecond.style.display = "none";
-    //         quotesContainerBoxThird.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxThird).getPropertyValue("display") == "block"){
-    
-    //         quotesContainerBoxThird.style.display = "none";
-    //         quotesContainerBoxFourth.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFourth).getPropertyValue("display") == "block"){
-    
-    //         quotesContainerBoxFourth.style.display = "none";
-    //         quotesContainerBoxFifth.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFifth).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxFifth.style.display = "none";
-    //         quotesContainerBoxFirst.style.display = "block";
-
-    //     }
-
-    // } 
-
-    // quotesContainerArrowsLeft.addEventListener('click', function () {  
-
-    //     if ((getComputedStyle(quotesContainerBoxFirst).getPropertyValue("display")) == "block"){
-
-    //         quotesContainerBoxFifth.style.display = "block";
-    //         quotesContainerBoxFirst.style.display = "none";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFifth).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxFifth.style.display = "none";
-    //         quotesContainerBoxFourth.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFourth).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxFourth.style.display = "none";
-    //         quotesContainerBoxThird.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxThird).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxThird.style.display = "none";
-    //         quotesContainerBoxSecond.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxSecond).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxSecond.style.display = "none";
-    //         quotesContainerBoxFirst.style.display = "block";
-
-    //     }
-    // })
-
-    // quotesContainerArrowsRight.addEventListener('click', function () {  
-        
-    //     if ((getComputedStyle(quotesContainerBoxFirst).getPropertyValue("display")) == "block"){
-
-    //         quotesContainerBoxFirst.style.display = "none";
-    //         quotesContainerBoxSecond.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxSecond).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxSecond.style.display = "none";
-    //         quotesContainerBoxThird.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxThird).getPropertyValue("display") == "block"){
-    
-    //         quotesContainerBoxThird.style.display = "none";
-    //         quotesContainerBoxFourth.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFourth).getPropertyValue("display") == "block"){
-    
-    //         quotesContainerBoxFourth.style.display = "none";
-    //         quotesContainerBoxFifth.style.display = "block";
-
-    //     } else if (getComputedStyle(quotesContainerBoxFifth).getPropertyValue("display") == "block"){
-
-    //         quotesContainerBoxFifth.style.display = "none";
-    //         quotesContainerBoxFirst.style.display = "block";
-
-    //     }
-
-    // })
 
 })
 
