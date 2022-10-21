b0baf82f4a73e5c41a41b5af8a19ebedd61e2c76
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-10-21 23:36:45 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -172,12 +172,14 @@ window.addEventListener('load',function () {
     });
 
     // Show Paragraph Aside Section Get Discount
+    let aside__title = document.querySelector(".aside__title");
     let asideBtn = document.querySelector(".aside__btn");
     let aside__paragraphOpacity = this.document.querySelector(".aside__paragraph--opacity");
 
     asideBtn.addEventListener("click" , ()=> {
         aside__paragraphOpacity.classList.add("active");
         asideBtn.classList.add("hidden");
+        aside__title.classList.add("active");
     });
 
 })
